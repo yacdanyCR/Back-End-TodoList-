5cbb718d90902317e8cd1fd71b162eac5461ba38
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-08 12:55:20 -0600

    Closing db connection

--- src/Controller/TaskController.js
+++ src/Controller/TaskController.js
@@ -4,38 +4,44 @@ const db = new Db();
 class TaskController {
 
     async getTask(req, res) {
+        const connection = await db.getConnection();
         try {
-            const connection = await db.getConnection();
             const result = await connection.query("SELECT * FROM Task");
             res.status(200).json(result);
         } catch (error) {
             res.status(400);
             console.error(error);
+        } finally {
+            connection.end();
         }
     }
 
     async addTask(req, res) {
+        const connection = await db.getConnection();
         try {
             const { task } = req.body;
             const taskObj = new TaskModel(task);
-            const connection = await db.getConnection();
             const result = await connection.query(`INSERT INTO Task (task) VALUES("${taskObj.getTask()}")`);
             res.status(200).json(result);
         } catch (error) {
             res.status(400);
             console.error(error);
+        } finally {
+            connection.end();
         }
     }
 
     async deleteTask(req, res) {
+        const connection = await db.getConnection();
         try {
             const { id } = req.params;
-            const connection = await db.getConnection();
             const result = await connection.query(`DELETE FROM Task WHERE id = ${id}`);
             res.status(200).json(result);
         } catch (error) {
             res.status(400);
             console.error(error);
+        } finally {
+            connection.end();
         }
     }
 }
