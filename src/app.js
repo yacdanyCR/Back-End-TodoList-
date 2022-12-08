acf68dd8cf139bba2df922f7fad91ef4ec5c0997
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-08 07:57:26 -0600

    Project Started

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const config = require('../config.js');
 const app = express()
+const cors = require('cors');
 const port = process.env.PORT || 3000
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Working")
