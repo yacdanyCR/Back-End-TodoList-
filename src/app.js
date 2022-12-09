7dfc5ddf023390a7c30db8123d3862a1e0b282d3
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-12-08 22:27:45 -0600

    Updating cors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const config = require('../config.js');
 const app = express()
-//const cors = require('cors');
+const cors = require('cors');
 const { taskRouter } = require('./routes/TaskRoute.js');
 
 const port = process.env.PORT || 3000
 
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 
 app.use(('/api/task'), taskRouter);
 app.get('/', (req, res) => {
